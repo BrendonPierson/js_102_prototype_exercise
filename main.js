cda44fee060226964acd449d7c41b66affc6b216
BrendonPierson <brendonpierson@gmail.com>
2015-07-22 11:25:16 -0500

    followed initial instructions

---
+++ main.js
@@ -0,0 +1,50 @@
+/*
+  .....  YOUR MISSION  .....
+  1. Author functions that define the Make
+      and Model of a Car and the Car itself.
+  2. Create three instances of your Car that
+      each have a different color.
+  3. Each Car you create should inherit the
+      make and model name.
+*/
+
+
+/*
+  Create a function, Make, that has a property
+   of makeName and give it the value of your
+   vehicle's make.
+ */
+
+Car.prototype.makeName = "Ford"
+
+
+/*
+Create a function, Model, to hold the 
+corresponding property and value
+*/
+Car.prototype.Model = "Model T"
+
+
+// What is the prototype of a Model?
+//Model T
+
+// Define a Car
+function Car(color) {
+  this.color = color
+}
+
+// What is the prototype of a Car?
+
+
+// Create the first car
+var firstCar = new Car("red");
+
+// Create the second car
+var secondCar = new Car("green");
+
+// Create the third car
+var thirdCar = new Car("blue");
+
+
+
+console.log(firstCar, secondCar, thirdCar);
